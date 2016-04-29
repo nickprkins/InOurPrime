f8087a8874f77756671ad7abd150989980a9f525
Nick Perkins <nickperkins@protonmail.com>
2016-04-29 14:34:18 -0400

    Initial Commit

---
+++ InOurPrime/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  InOurPrime
+//
+//  Created by Nick Perkins on 4/29/16.
+//  Copyright © 2016 Nick Perkins. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ InOurPrime/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  InOurPrime
+//
+//  Created by Nick Perkins on 4/29/16.
+//  Copyright © 2016 Nick Perkins. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
